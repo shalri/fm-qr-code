9687a7511402caddb05f4ae9611e3723fec125b3
shalri <fsaonlinework777@gmail.com>
2024-05-02 11:49:37 +0800

    Integrating resources from challenge

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,28 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "qr-white": "hsl(228, 45%, 44%)",
+        "qr-light-gray": "hsl(212, 45%, 89%)",
+        "qr-gray-blue": "hsl(220, 15%, 55%)",
+        "qr-dark-blue": "hsl(218, 44%, 22%)",
+      },
+      fontSize: {
+        body: "15px",
+      },
+      fontFamily: {
+        outfit: ["Outfit", "sans-serif"],
+      },
+      fontWeight: {
+        normal: "400",
+        bold: "700",
+      },
+      screens: {
+        mobile: "375px",
+        desktop: "1440px",
+      },
+    },
   },
   plugins: [],
 };
